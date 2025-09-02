4890d34aa7eccc59470df71edc4dc82a4ab499f0
ErnestQA <ernest12qa@gmail.com>
2025-09-02 11:31:12 +0200

    Add Automated-Testing folder

---
+++ tests/test_data.py
@@ -0,0 +1,9 @@
+USERS = {
+    "valid": {"email": "test@example.com", "password": "mypassword123"},
+    "user2": {"email": "wrong1@example.com", "password": "pass1"},
+    "user3": {"email": "wrong2@example.com", "password": "pass2"}
+}
+
+VALID_USER = USERS["valid"]
+
+INVALID_USERS = {k: v for k, v in USERS.items() if k != "valid"}
